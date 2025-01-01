d570c398027eb7a0253c3ebb54a04c259192376b
Kinfash2000 <117234201+Kinfash2000@users.noreply.github.com>
2025-01-01 17:40:51 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "my-dev-tf-state-btk"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "MY-Db1-Table"
   }
 }
